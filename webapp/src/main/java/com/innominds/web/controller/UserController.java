8a160ef4946049a14d2b687dfb62b89e03ef40a8
unknown <nboddu@IM-LP-104.innomindshyd.com>
2013-12-11 15:43:32 +0530

    main added

--- webapp/src/main/java/com/innominds/web/controller/UserController.java
+++ webapp/src/main/java/com/innominds/web/controller/UserController.java
@@ -1,5 +1,9 @@
 package com.innominds.web.controller;
 
-public class UserController {
 
+public class UserController {
+	
+	public static void main(String[] args) {
+		System.out.println("UserController.main()");
+	}
 }
