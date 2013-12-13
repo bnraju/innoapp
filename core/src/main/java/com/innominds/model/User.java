d38d862f94d282d5b0be1ed3f4354d90fdf24f51
bnraju <n@g@r@ju@9>
2013-12-13 13:34:00 +0530

    Entity updated

--- core/src/main/java/com/innominds/model/User.java
+++ core/src/main/java/com/innominds/model/User.java
@@ -1,162 +1,114 @@
 package com.innominds.model;
 
-import static javax.persistence.GenerationType.IDENTITY;
-
 import java.io.Serializable;
+import java.util.Collection;
 import java.util.Date;
 
 import javax.persistence.Column;
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 
-
-
-@Entity()
-@Table ( name = "user" , uniqueConstraints = {@UniqueConstraint(columnNames={"UserID"})})
+@Entity(name = "USER")
+// @Table ( name = "user" , uniqueConstraints =
+// {@UniqueConstraint(columnNames={"UserID"})})
 public class User implements Serializable {
-
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	
-	
-	private String userID;
-	private String userName;
-	private String firstName;
-	private String lastName;
-	private Date dob;
-	private String designation;
-	private String password ;
-	private String salt;
-	
-	/*private Address address;
-	private Role role;*/
-	
-	// Setter and Getters
-	public void setUserID ( String uid )
-	{
-		this.userID = uid;
-	}
-	
 	@Id
-	@GeneratedValue ( strategy = GenerationType.AUTO)
-	@Column ( name = "UserID" , unique = true , nullable = false)	
-	
-	public String getUserID  ( )
-	{
-		return this.userID;
-		
+	@GeneratedValue
+	@Column(name="ID")
+	private int userID;
+	@Column(nullable = false, unique = true)
+	private String userName;
+	@Column(name="FIRST_NAME",nullable = false)
+	private String firstName;
+	@Column(name="LAST_NAME",nullable = false)
+	private String lastName;
+	@Column(name="DOB",nullable = false)
+	private Date dob;
+	@Column(name="DESIGNATION")
+	private String designation;
+	@Column(name="PASSWORD",length = 150, nullable = false)
+	private String password;
+	@Column(name="SALT",nullable = false)
+	private String salt;
+	@Embedded
+	private Address address;
+	@ManyToMany
+	@JoinTable(name="USER_ROLE",
+	joinColumns=@JoinColumn(name="USER_ID"),
+	inverseJoinColumns=@JoinColumn(name="ROLE_ID"))
+	private Collection<Role> roles;
+
+	public int getUserID() {
+		return userID;
 	}
-	
-	public void setUserName ( String username )
-	{
-		this.userName = username;
+
+	public void setUserID(int userID) {
+		this.userID = userID;
 	}
-	
-	@Column ( name = "UserName" , unique = true , nullable = false)
-	public String getUserName ( )
-	{
-		return this.userName;
+
+	public String getUserName() {
+		return userName;
 	}
-	
-	public void setFirstName ( String firstname )
-	{
-		this.firstName = firstname;
+
+	public void setUserName(String userName) {
+		this.userName = userName;
 	}
-	@Column ( name = "FirstName" , unique = false , nullable = true)
-	public String getFirstName ( )
-	{
-		return this.firstName;
+
+	public String getFirstName() {
+		return firstName;
 	}
-	
-	public void setLastName ( String lastname )
-	{
-		this.lastName = lastname;
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
 	}
-	
-	@Column ( name = "LastName" , unique = false , nullable = true)
-	public String getLastName ( )
-	{
-		return this.lastName;
+
+	public String getLastName() {
+		return lastName;
 	}
-	
-	public void setDesignation ( String designation )
-	{
-		this.designation = designation;
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
 	}
-	@Column ( name = "Designation" , unique = false , nullable = true)
-	public String getDesignation ( )
-	{
-		return this.designation ;
+
+	public Date getDob() {
+		return dob;
 	}
-	
-	public void setPassword ( String password )
-	{
-		this.password = password;
-	}
-	
-	@Column ( name = "Password" , unique = false , nullable = false)
-	public String getPassword ( )
-	{
-		return this.password;
-	}
-	
-	public void setDob ( Date dob)
-	{
+
+	public void setDob(Date dob) {
 		this.dob = dob;
 	}
-	@Column ( name = "DOB" , unique = false , nullable = true)
-	public Date getDob ( )
-	{
-		return this.dob;
+
+	public String getDesignation() {
+		return designation;
 	}
-	
-	public void setSalt ( String salt )
-	{
+
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getSalt() {
+		return salt;
+	}
+
+	public void setSalt(String salt) {
 		this.salt = salt;
-	}
-	
-	@Column ( name = "Salt" , unique = false , nullable = true)
-	public String getSalt ( )
-	{
-		return this.salt;
-	}
-	
-	/*public void setAddress ( Address address )
-	{
-		this.address = address;
-	}
-	
-	
-	public Address getAddress ( )
-	{
-		return this.address;
-	}
-	
-	
-	public void setRole ( Role role)
-	{
-		this.role = role;
-	}
-	
-	
-	public Role getRole ( )
-	{
-		return this.role;
-	}*/
-	
-	public User() {
-		// TODO Auto-generated constructor stub
-	}
-	
-	public String toString ( ) 
-	{
-		return "Firstname " + this.firstName ;
 	}
 
 }
